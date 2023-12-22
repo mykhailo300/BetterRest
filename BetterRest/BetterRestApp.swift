a2cd3d636347bf07a046dd35d07bdb1f9f4f5596
Mykhailo <dmytrivmykhailo2@gmail.com>
2023-12-22 21:30:09 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Михайло Дмитрів on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
